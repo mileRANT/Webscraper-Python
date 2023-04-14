f55025bf9c4838126bdc1ac14c094f30e260b5ab
mileRANT <liem.htran@outlook.com>
2023-04-14 16:00:11 -0400

    Importing beautiful soup. Webscraping library from past project.

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+import requests
+from bs4 import BeautifulSoup
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+# Write your code below this line 👇
+
+response = requests.get(URL)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+
+all_movies = soup.find_all(name="h3", class_="title")
+
+movie_titles = [movie.getText() for movie in all_movies]
+movies = movie_titles[::-1]
+
+with open("movies.txt", mode="w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+'''
+FAQ: Empire's website has changed!
 
+When this lesson was created, I used this URL for the project: 
+URL = "https://www.empireonline.com/movies/features/best-movies-2/"
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+However, Empire has since changed their website. You can see this when you inspect the movie title elements. 
+You'll see that the h3 with the class "title" is no longer there. 
+To use exactly the same code as per the solution, we can use a cached version of the website from the Internet Archive's Wayback Machine.
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+'''
