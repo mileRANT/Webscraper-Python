4e9bdbcc20b7c7b8624ef5326b60bd242d1d662c
mileRANT <liem.htran@outlook.com>
2023-04-14 16:10:51 -0400

    Adjusted URL and tag information to enable it to look for current anime this season.

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
-
-# Write your code below this line 👇
+URL = "https://myanimelist.net/anime/season"
 
 response = requests.get(URL)
 website_html = response.text
 
 soup = BeautifulSoup(website_html, "html.parser")
 
-all_movies = soup.find_all(name="h3", class_="title")
+animes_season = soup.find_all(name="h2", class_="h2_anime_title")
 
-movie_titles = [movie.getText() for movie in all_movies]
-movies = movie_titles[::-1]
+anime_titles = [anime.getText() for anime in animes_season]
+animes = anime_titles[::-1]
 
-with open("movies.txt", mode="w") as file:
-    for movie in movies:
-        file.write(f"{movie}\n")
+with open("anime.txt", mode="w", encoding='utf-8') as file:
+    for anime in animes:
+        file.write(f"{anime}\n")
 
-
-'''
-FAQ: Empire's website has changed!
-
-When this lesson was created, I used this URL for the project: 
-URL = "https://www.empireonline.com/movies/features/best-movies-2/"
-
-However, Empire has since changed their website. You can see this when you inspect the movie title elements. 
-You'll see that the h3 with the class "title" is no longer there. 
-To use exactly the same code as per the solution, we can use a cached version of the website from the Internet Archive's Wayback Machine.
-
-'''
